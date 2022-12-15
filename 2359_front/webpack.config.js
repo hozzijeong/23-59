310ae27497c4846bc422b58b9641db0a1133390a
hozzijeong <ghwls0905@naver.com>
2022-12-15 13:03:03 +0900

    Chore: Webpack, babel-loader 설치

---
+++ 2359_front/webpack.config.js
@@ -0,0 +1,46 @@
+import { resolve as _resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export const entry = _resolve(__dirname, '..', './src/index.tsx');
+export const resolve = {
+  extensions: ['.tsx', '.ts', '.js'],
+};
+export const module = {
+  rules: [
+    {
+      test: /\.(ts|js)x?$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+      type: 'asset/inline',
+    },
+  ],
+};
+
+export const output = {
+  path: _resolve(__dirname, '..', './build'),
+  filename: 'bundle.js',
+};
+
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: _resolve(__dirname, '..', './src/index.html'),
+  }),
+];
+
+export const mode = 'development';
+export const stats = 'errors-only';
