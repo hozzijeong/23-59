10c68efe8d0c3824442286e92cd697571ed4d133
hozzijeong <ghwls0905@naver.com>
2022-12-15 13:06:11 +0900

    Chore: postcss 설치 및 tailwind & postcss config 파일 설정

--- 2359_front/tailwind.config.js
+++ 2359_front/tailwind.config.js
@@ -1,31 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,tsx,ts,jsx}'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [],
   theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
-    },
-    colors: {
-      primary: '#bdb9b9',
-      'white-brown': '#efebeb',
-      blue: '#1fb6ff',
-      purple: '#7e5bef',
-      pink: '#ff49db',
-      orange: '#ff7849',
-      green: '#13ce66',
-      yellow: '#ffc82c',
-      'gray-dark': '#273444',
-      gray: '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {},
   },
   plugins: [],
