b948c5c9893d69d64512d4620252bcd587d8a7d4
hozzijeong <ghwls0905@naver.com>
2022-12-15 11:45:21 +0900

    Chore: babel, tailwind 라이브러리 설치

--- 2359_front/src/components/Header.tsx
+++ 2359_front/src/components/Header.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
 
-const HeaderContainer = styled.div`
-  width: 100%;
-  height: 115px;
-  background-color: #bdb9b9;
+const HeaderContainer = tw.div`
+ml-3
 `;
 
-const HeaderContent = styled.div`
-  width: 1024px;
-  margin: 0 auto;
+const HeaderContent = tw.div`
+  bg-lg
 `;
 
 function Header() {
