0830c858b9e8cdd04e4c86896dfc108d75a47e69
hozzijeong <ghwls0905@naver.com>
2022-12-16 10:32:15 +0900

    Design: Header 기본 틀 잡기

--- 2359_front/src/components/Header.tsx
+++ 2359_front/src/components/Header.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
 import tw from 'tailwind-styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const HeaderContainer = tw.div`
-  flex
-  items-center
-  justify-center
-  flex-col
   w-full
-  bg-primary
+  h-[115px]
+  bg-primaryDark
 `;
 
 const HeaderContent = tw.div`
+  max-w-screen-lg
+  flex
+  justify-between
+  items-center
+  my-0
+  mx-auto
+  pt-[25px]
+`;
 
+const Logo = tw.button`
+  h-[6rem]
+`;
+
+const HeaderRightContainer = tw.div`
+  flex
+  justify-around
+  w-[200px]
 `;
 
 function Header() {
+  const nav = useNavigate();
+
+  const logoClickHandler = () => {
+    nav('/user/main');
+  };
+
   return (
     <HeaderContainer>
-      <HeaderContent>Header!</HeaderContent>
-      <HeaderContent>Header222</HeaderContent>
+      <HeaderContent>
+        <Logo onClick={logoClickHandler}>Logo</Logo>
+        <HeaderRightContainer>
+          <button type="button">myPage</button>
+          <button type="button">logout</button>
+        </HeaderRightContainer>
+      </HeaderContent>
     </HeaderContainer>
   );
 }
