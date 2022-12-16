d8be78e195511a14dad8ac2dda4ee8665bd52f42
hozzijeong <ghwls0905@naver.com>
2022-12-16 15:10:19 +0900

    Feat: 다이어리 checkbox 및 목차 생성

--- 2359_front/src/pages/Diary.tsx
+++ 2359_front/src/pages/Diary.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import styled from 'styled-components';
 import tw from 'tailwind-styled-components';
 
 const DiarySection = tw.section`
@@ -14,7 +15,8 @@ const HeadContent = tw.div`
 const Content = tw.div`
   max-w-screen-md
   my-0
-  mx-auto 
+  mx-auto
+  text-[1.8rem]
 `;
 
 const Title = tw.p`
@@ -27,32 +29,78 @@ const RelativeDiv = tw.div`
   relative
 `;
 
-const AbsoluteDiv = tw.div`
-  absolute  
+interface AbsoluteProps {
+  isLeft: boolean;
+}
+
+const AbsoluteDiv = tw.div<AbsoluteProps>`
+  absolute
+  ${(props) => (props.isLeft ? 'left-[-12rem]' : 'right-[12rem]')}
 `;
 
-const TEMP_DATA = [
-  { id: '1', content: 'To-Do-List' },
-  { id: '2', content: '오늘의 질문' },
-  { id: '3', content: '감정 일기' },
-  { id: '4', content: '가계부' },
+const Ullists = styled.ul`
+  li {
+    display: flex;
+    align-content: center;
+    font-size: 1.4rem;
+  }
+`;
+
+const FixedUl = tw(Ullists)`
+  fixed
+`;
+
+interface ContentOption {
+  id: string;
+  content: string;
+  isChecked: boolean;
+}
+
+const TEMP_DATA: ContentOption[] = [
+  { id: '1', content: 'To-Do-List', isChecked: false },
+  { id: '2', content: '오늘의 질문', isChecked: false },
+  { id: '3', content: '감정 일기', isChecked: false },
+  { id: '4', content: '가계부', isChecked: false },
 ];
 
 function Diary() {
+  const [contentOptions, setContentOptions] = useState<ContentOption[]>(TEMP_DATA);
+  const [tableContents, setTableContents] = useState<string[]>(['To-Do-List', '오늘의 질문', '감정 일기', '가계부']);
+  const optionHandler = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { id } = event.target;
+      setContentOptions((currentOptions) =>
+        currentOptions.map((options) => (options.id === id ? { ...options, isChecked: true } : options))
+      );
+    },
+    [contentOptions]
+  );
+
   return (
     <DiarySection>
       <HeadContent>
         <Title>title</Title>
         <RelativeDiv>
-          <AbsoluteDiv>
-            <ul>
-              {TEMP_DATA.map(({ id, content }) => (
+          <AbsoluteDiv isLeft>
+            <FixedUl>
+              {contentOptions.map(({ id, content }) => (
                 <li key={id}>
-                  <input id={id} type="checkbox" value={content} />
+                  <input className="text-2rem" id={id} type="checkbox" value={content} onChange={optionHandler} />
                   <label htmlFor={id}>{content}</label>
                 </li>
               ))}
-            </ul>
+            </FixedUl>
+          </AbsoluteDiv>
+        </RelativeDiv>
+        <RelativeDiv>
+          <AbsoluteDiv isLeft={false}>
+            <FixedUl>
+              {tableContents.map((data) => (
+                <li>
+                  <a href={`#${data}`}>{data}</a>
+                </li>
+              ))}
+            </FixedUl>
           </AbsoluteDiv>
         </RelativeDiv>
       </HeadContent>
