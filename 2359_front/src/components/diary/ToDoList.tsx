ba632f6ffad5a53734c94a2bfe2e5719e8d3092a
hozzijeong <ghwls0905@naver.com>
2022-12-17 08:53:24 +0900

    Feat: 리스트에 임시로 id(날짜) 부여

--- 2359_front/src/components/diary/ToDoList.tsx
+++ 2359_front/src/components/diary/ToDoList.tsx
@@ -12,13 +12,36 @@ function TodoList() {
       alert('한 글자 이상 입력해주세요!');
       return;
     }
-    setCurTodo((cur) => ({ ...cur, todos: [...cur.todos, { isChecked: false, todoContent: todoInput }] }));
+    const date = Date.now();
+    setCurTodo((cur) => ({
+      ...cur,
+      todos: [...cur.todos, { id: date.toString(10), isChecked: false, todoContent: todoInput }],
+    }));
     setTodoInput('');
   };
 
   const changeTodoInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setTodoInput(value);
+  };
+
+  // typescript에서 li에 onClick event를 설정하고 싶은데 그게 되지 않음. 그 이유는 eslint의
+  // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/4abc751d87a8491219a9a3d2dacd80ea8adcb79b/docs/rules/click-events-have-key-events.md
+  // 때문에 안된다고함
+  const todoClickHandler = (event: React.MouseEvent<HTMLInputElement | HTMLButtonElement | HTMLLIElement>) => {
+    console.log(event.target, event.currentTarget);
+  };
+
+  const changeTodoCheckHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id } = event.target;
+    const { todos } = curTodo;
+    const updateTodos = todos.map((todo) => (todo.id === id ? { ...todo, isChecked: !todo.isChecked } : { ...todo }));
+
+    setCurTodo((cur) => ({ ...cur, todos: updateTodos }));
+  };
+
+  const todoDeleteHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    return null;
   };
 
   return (
@@ -31,11 +54,14 @@ function TodoList() {
       </div>
       <div>
         <ul>
-          {curTodo.todos.map(({ isChecked, todoContent }) => {
+          {curTodo.todos.map(({ id, isChecked, todoContent }) => {
             return (
-              <li>
-                <input id={todoContent} type="checkbox" checked={isChecked} />
-                <label htmlFor={todoContent}>{todoContent}</label>
+              <li key={id}>
+                <input id={id} type="checkbox" defaultChecked={isChecked} onChange={changeTodoCheckHandler} />
+                <label htmlFor={id}>{todoContent}</label>
+                <button onClick={todoDeleteHandler} type="button">
+                  삭제하기
+                </button>
               </li>
             );
           })}
