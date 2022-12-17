c59ddc519660f9ccf19825cb6195aa7ac2f9fa69
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2022-12-17 11:24:00 +0900

    Feat : 회원가입 페이지 생성

--- 2359_front/src/pages/Routes.tsx
+++ 2359_front/src/pages/Routes.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes as ReactRouterRoutes, Navigate } from 'react-router-dom';
 import Diary from 'pages/Diary';
 import Landing from 'pages/Landing';
+import SignUp from './SignUp';
 
 export default function Routes() {
   return (
     <ReactRouterRoutes>
       <Route path="/diary" element={<Diary />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/" element={<Landing />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </ReactRouterRoutes>
