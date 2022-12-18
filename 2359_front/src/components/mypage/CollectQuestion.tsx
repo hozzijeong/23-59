11f4e439c8af0c9487ea325636c98f8ef087b8e6
Chris <dnchks08@gmail.com>
2022-12-18 18:18:47 +0900

    Feat: 질문 모아보기 기능 추가
    isSelect state를 객체로 선언하여 data의 값을 key: boolean 형태로 할당,
    클릭시 className을 바꿔주는 동시에 setState를 진행

--- 2359_front/src/components/mypage/CollectQuestion.tsx
+++ 2359_front/src/components/mypage/CollectQuestion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import tw from 'tailwind-styled-components';
 
 const tagData: string[] = ['#슬픔', '#기쁨', '#행복', '#죽음', '#공부', '#연애', '#사랑', '#고민', '#걱정'];
@@ -7,32 +7,31 @@ const TagButtons = tw.button`
   mr-3
 `;
 
-const selectBtn = `
+const selectBtnClass = `
   inline-block px-2.5 py-1 bg-blue-600 text-white font-medium text-xs leading-tight rounded-xl shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg
   mr-3
 `;
 
-const nonSelectBtn = `
-  inline-block px-2.5 py-1 text-blue font-medium text-xs leading-tight rounded-xl shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg
+const nonSelectBtnClass = `
+  inline-block px-2.5 py-1 text-blue font-medium text-xs leading-tight rounded-xl shadow-md hover:bg-blue-700 hover:shadow-lg  active:bg-blue-800 active:shadow-lg
   mr-3
 `;
 
-// interface IData {
-//   string: boolean,
-// }
+interface IData {
+  [key: string]: boolean;
+}
 
-const newData: any = {};
-const setData = () => {
+const newData: IData = {};
+const setData = (): void => {
   tagData.forEach((item) => {
     newData[item] = true;
   });
 };
-
+setData();
+console.log('newData', newData);
 // 생각해볼 키워드!
 // 그냥 Object면... 내가 누른거의 key값을 받아오면 그걸로 파악할수있지 않나?
 // 객체가 아니라 배열로 관리 하면 괜찮?
-
-setData();
 
 function CollectQuestion() {
   const [isSelect, setIsSelect] = useState(newData);
@@ -41,8 +40,15 @@ function CollectQuestion() {
     const newSelect = { ...isSelect };
     newSelect[item] = !newSelect[item];
     setIsSelect(newSelect);
-    // console.log(newData);
+    console.log('newSelect', newSelect);
   };
+
+  function selectedTag(ele: string): string {
+    if (isSelect[ele]) {
+      return selectBtnClass;
+    }
+    return nonSelectBtnClass;
+  }
 
   return (
     <div>
@@ -50,12 +56,7 @@ function CollectQuestion() {
       <div>
         {tagData.map((item) => {
           return (
-            <TagButtons
-              className={isSelect ? selectBtn : nonSelectBtn}
-              key={item}
-              type="button"
-              onClick={() => handleTagName(item)}
-            >
+            <TagButtons className={selectedTag(item)} key={item} type="button" onClick={() => handleTagName(item)}>
               {item}
             </TagButtons>
           );
