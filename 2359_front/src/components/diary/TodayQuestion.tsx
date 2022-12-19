34c3fab6e7d8bf22ced7362da6bb1ab00c19c0b4
hozzijeong <ghwls0905@naver.com>
2022-12-19 23:25:01 +0900

    Refactor: Diary 오늘의 질문 전역 상태 분리 및 변경

--- 2359_front/src/components/diary/TodayQuestion.tsx
+++ 2359_front/src/components/diary/TodayQuestion.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { useRecoilState } from 'recoil';
-import { diaryAtom } from 'recoil/diaryAtom';
+import { questionAnswer } from 'recoil/diaryAtom';
 
 function TodayQuestion() {
-  const [answer, setAnswer] = useRecoilState(diaryAtom);
+  const [answer, setAnswer] = useRecoilState(questionAnswer);
 
   const answerChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = event.target;
-    setAnswer((cur) => ({ ...cur, questionAnswer: value }));
+    setAnswer(value);
   };
 
   return (
     <div>
       <p>오늘 하루 어떠셨나요?</p>
-      <textarea value={answer.questionAnswer} onChange={answerChangeHandler} />
+      <textarea value={answer} onChange={answerChangeHandler} />
     </div>
   );
 }
