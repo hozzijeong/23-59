0cb8de5c486bc0294c9fd75073cf2a1e1036a321
yoonbly <81467705+yoonbly@users.noreply.github.com>
2022-12-19 16:30:06 +0900

    Feat:버튼 컴포넌트 생성 및 랜딩 페이지 삭제
    feat: 버튼 컴포넌트(공통 컴포넌트)생성
    docs: 랜딩 페이지 삭제(기획에서 제외)

---
+++ 2359_front/src/components/Button.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import tw from 'tailwind-styled-components';
+
+interface BtnProps {
+  children?: React.ReactNode;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  btntype?: 'basic' | 'save' | 'cancel';
+  btnsize?: 'sm' | 'base' | 'lg';
+  disabled?: boolean;
+}
+
+function Button({ children, onClick, btntype, btnsize, disabled }: BtnProps) {
+  return (
+    <MyButton onClick={onClick} disabled={disabled} btntype={btntype} btnsize={btnsize}>
+      {children}
+    </MyButton>
+  );
+}
+
+export default Button;
+
+Button.defaultProps = {
+  children: '',
+  onClick: null,
+  btntype: 'basic',
+  btnsize: 'base',
+  disabled: false,
+};
+
+const MyButton = tw.button<BtnProps>`
+border-none
+rounded-lg 
+hover:shadow-lg
+ease-linear 
+transition-all 
+duration-150
+${(props) => props.btntype === 'basic' && `bg-primaryLight`}
+${(props) => props.btntype === 'save' && `bg-primaryDark`}
+${(props) => props.btntype === 'cancel' && `bg-primary`}
+
+${(props) => props.btnsize === 'sm' && `px-3 py-1 text-sm`}
+${(props) => props.btnsize === 'base' && `px-3 py-1 text-base`}
+${(props) => props.btnsize === 'lg' && `px-4 py-2 text-lg`}
+`;
