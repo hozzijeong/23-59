52189d8f8845d026e0503b53bf5d7f09830da940
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2022-12-22 00:21:07 +0900

    feat : 회원가입 api 연동

--- 2359_front/src/hooks/useRegister.ts
+++ 2359_front/src/hooks/useRegister.ts
@@ -1,31 +1,25 @@
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { RegisterFormValue } from 'types/interfaces';
+import { useCallback } from 'react';
+import { baseAxios } from '../api';
 
-/* eslint-disable no-useless-escape */
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable jsx-a11y/label-has-associated-control */
-
-export const useRegister = (Data: RegisterFormValue) => {
+const useRegister = () => {
   const navigate = useNavigate();
-  axios
-    .post(
-      `http:localhost:8000/api/user/register/
-    `,
-      Data,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    )
-    .then((res) => {
-      const data = res;
-      console.log(data);
-      alert('가입 완료 됐습니다.');
-      navigate('/login');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+
+  const registerRequest = useCallback((Data: RegisterFormValue) => {
+    baseAxios
+      .post(`api/user/register`, Data)
+      .then((res) => {
+        console.log(res);
+        alert('가입 완료 됐습니다.');
+        navigate('/login');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return { registerRequest };
 };
+
+export default useRegister;
