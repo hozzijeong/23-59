ac9a4aa3c6c0cc286f500b248c72ed57b371004a
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2022-12-22 00:20:04 +0900

    Fix : Cors 에러 해결 body : network 에러는 cors 설정을 안해줘서 생긴 에러다.

--- 2359_back/src/index.ts
+++ 2359_back/src/index.ts
@@ -15,7 +15,8 @@ app.use(express.json());
 // Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 app.use('/api', userRouter);
 app.use('/api', userOptionRouter);
