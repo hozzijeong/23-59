caf0ddf50b1f8dba84ff4b1cff31a8747cdb9b53
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2022-12-26 13:42:23 +0900

    Refactor : MR 코드리뷰 반영 body : regex  => EMAIL_REGEX로 변수명 변경 SC. => 코드 컨벤션 반영 api => header에 토큰 보내는 코인이 많으니 headerAxios를 만들었습니다.

--- 2359_front/src/components/login/Loginform.tsx
+++ 2359_front/src/components/login/Loginform.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { LoginFormValue } from '../../types/interfaces';
-import * as SC from '../signup/FormStyled';
-import { emailCheck } from '../../utilities/regex';
+import {
+  ErrorMesg,
+  SubmitButton,
+  FormInput,
+  FormLabel,
+  FormTitle,
+  Form,
+  Container,
+  SignUpLink,
+} from '../signup/FormStyled';
+import { EMAIL_REGEX } from '../../utilities/regex';
 import useLogin from '../../hooks/useUserLogin';
 /* eslint-disable react/jsx-props-no-spreading */
 
@@ -15,36 +24,32 @@ function Loginform() {
     formState: { errors },
   } = useForm<LoginFormValue>();
 
-  const OnSubmit: SubmitHandler<LoginFormValue> = (data) => {
+  const onSubmit: SubmitHandler<LoginFormValue> = (data) => {
     loginRequest(data);
   };
 
   return (
-    <SC.Container>
-      <SC.Form onSubmit={handleSubmit(OnSubmit)}>
-        <SC.FormTitle>로그인</SC.FormTitle>
-        <SC.FormLabel>이메일</SC.FormLabel>
-        <SC.FormInput type="email" {...register('email', { required: true, pattern: emailCheck })} />
-        {errors.email && errors.email.type === 'required' && <SC.ErrorMesg>이메일을 입력해주세요.</SC.ErrorMesg>}
-        {errors.email && errors.email.type === 'pattern' && <SC.ErrorMesg>올바른 이메일을 입력해주세요.</SC.ErrorMesg>}
-        <SC.FormLabel>비밀번호</SC.FormLabel>
-        <SC.FormInput
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormTitle>로그인</FormTitle>
+        <FormLabel>이메일</FormLabel>
+        <FormInput type="email" {...register('email', { required: true, pattern: EMAIL_REGEX })} />
+        {errors.email && errors.email.type === 'required' && <ErrorMesg>이메일을 입력해주세요.</ErrorMesg>}
+        {errors.email && errors.email.type === 'pattern' && <ErrorMesg>올바른 이메일을 입력해주세요.</ErrorMesg>}
+        <FormLabel>비밀번호</FormLabel>
+        <FormInput
           autoComplete="new-password"
           {...register('password', { required: true, minLength: 6 })}
           type="password"
         />
-        {errors.password && errors.password.type === 'required' && (
-          <SC.ErrorMesg>비밀번호를 입력해주세요.</SC.ErrorMesg>
-        )}
-        {errors.password && errors.password.type === 'minLength' && (
-          <SC.ErrorMesg>6자 이상으로 입력해주세요.</SC.ErrorMesg>
-        )}
-        <SC.SubmitButton type="submit">로그인</SC.SubmitButton>
-        <SC.SignUpLink>
+        {errors.password && errors.password.type === 'required' && <ErrorMesg>비밀번호를 입력해주세요.</ErrorMesg>}
+        {errors.password && errors.password.type === 'minLength' && <ErrorMesg>6자 이상으로 입력해주세요.</ErrorMesg>}
+        <SubmitButton type="submit">로그인</SubmitButton>
+        <SignUpLink>
           <Link to="/signup">Signup</Link>
-        </SC.SignUpLink>
-      </SC.Form>
-    </SC.Container>
+        </SignUpLink>
+      </Form>
+    </Container>
   );
 }
 
