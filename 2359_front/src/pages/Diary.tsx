688e20da9d0daea9a63d9738b258cff12bebddf3
hozzijeong <ghwls0905@naver.com>
2022-12-27 20:33:25 +0900

    Refactor: Diary-> body 에 중복코드 제거

--- 2359_front/src/pages/Diary.tsx
+++ 2359_front/src/pages/Diary.tsx
@@ -18,13 +18,8 @@ import { useTodayDiary } from 'hooks/useTodayDiary';
 import { useSWRConfig } from 'swr';
 import { createDiary, deleteDiary, updateDiary } from 'api';
 import { convertDiaryTitleToKor } from 'utilities/convertDiaryTitle';
-import { DiaryBodyProps, OptionCheckedProps } from 'types/interfaces';
-import {
-  INITIAL_BODY,
-  INITIAL_CONTENT_OPTIONS,
-  INITIAL_DIARY_INFO,
-  INITIAL_TODAY_DIARY,
-} from 'utilities/initialValues';
+import { DiaryBodyProps } from 'types/interfaces';
+import { INITIAL_BODY, INITIAL_DIARY_INFO } from 'utilities/initialValues';
 import { useRecoilValue } from 'recoil';
 import { accountTableAtom, emotionAtom, questionAtom, todayDiaryAtom, todayTodo } from 'recoil/diaryAtom';
 
@@ -106,23 +101,25 @@ function Diary() {
     const { _id, selectedDate } = diaryInfo;
 
     const body: DiaryBodyProps = contentOptions.reduce((acc, { title, isChecked }) => {
+      const checkOption = { ...acc.checkOption, [title]: isChecked };
+      const falseOption = { ...acc.checkOption, [title]: false };
       switch (title) {
         case OptionEnums.ACCOUNT_BOOK:
           if (isChecked && account.length !== 0) {
-            return { ...acc, account, checkOption: { ...acc.checkOption, [title]: isChecked } };
+            return { ...acc, account, checkOption };
           }
-          return { ...acc, checkOption: { ...acc.checkOption, [title]: false } };
+          return { ...acc, checkOption: falseOption };
         case OptionEnums.DIARY:
           if (isChecked && diary.title !== '') {
             // 타이틀만은 무조건 받기
-            return { ...acc, diary, checkOption: { ...acc.checkOption, [title]: isChecked } };
+            return { ...acc, diary, checkOption };
           }
-          return { ...acc, checkOption: { ...acc.checkOption, [title]: false } };
+          return { ...acc, checkOption: falseOption };
         case OptionEnums.EMOTION:
           if (isChecked && emotion !== null) {
-            return { ...acc, emotion, checkOption: { ...acc.checkOption, [title]: isChecked } };
+            return { ...acc, emotion, checkOption };
           }
-          return { ...acc, checkOption: { ...acc.checkOption, [title]: false } };
+          return { ...acc, checkOption: falseOption };
         case OptionEnums.TODAY_QUESTION:
           if (isChecked && qna.answer !== '') {
             return {
@@ -131,15 +128,15 @@ function Diary() {
                 questionId: qna._id,
                 answer: qna.answer,
               },
-              checkOption: { ...acc.checkOption, [title]: isChecked },
+              checkOption,
             };
           }
-          return { ...acc, checkOption: { ...acc.checkOption, [title]: false } };
+          return { ...acc, checkOption: falseOption };
         case OptionEnums.TODO_LIST:
           if (isChecked && todo.length !== 0) {
-            return { ...acc, todo, checkOption: { ...acc.checkOption, [title]: isChecked } };
+            return { ...acc, todo, checkOption };
           }
-          return { ...acc, checkOption: { ...acc.checkOption, [title]: false } };
+          return { ...acc, checkOption: falseOption };
 
         default: {
           return { ...acc };
