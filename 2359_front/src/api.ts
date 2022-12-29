17689ef467c2f72c75f1b7891d5d11b588646a19
hozzijeong <ghwls0905@naver.com>
2022-12-29 14:15:18 +0900

    Refactor: BASE_URL 배포 url로 변경

--- 2359_front/src/api.ts
+++ 2359_front/src/api.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import { DiaryBodyProps } from 'types/interfaces';
 
+const BASE_URL = 'http://ec2-35-79-109-245.ap-northeast-1.compute.amazonaws.com';
 const baseAxios = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
 const headerAxios = (token: string) => {
   return axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: BASE_URL,
     withCredentials: true,
     headers: { Authorization: `Bearer ${token}` },
   });
